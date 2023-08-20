75fcc5bcf0772594350b479e2b2fe5072058273e
Peter Shahin <petershahin00@gmail.com>
2023-08-21 01:53:52 +0300

    edit portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,21 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        newBlack: '#151515',
+        newvoilet: '#494c99',
+        darkgray: '#242424',
+        gray: '#d9d9d9'
+      },
+      fontFamily: {
+        custom: ['Hanken Grotesk', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
