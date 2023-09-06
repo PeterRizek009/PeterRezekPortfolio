4ede2c851a72a994de25962096ab62144c6b03c7
Peter Shahin <petershahin00@gmail.com>
2023-09-06 18:32:29 +0300

    edit

--- src/footer/footer.jsx
+++ src/footer/footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
   return (
     <footer className='w-full mt-8 relative'>
 
-      <div className='flex flex-col md:flex-row justify-between items-center md:items-start md:max-w-[1200px] lg:max-w-[1600px]  mx-auto'>
+      <div className='flex flex-col md:flex-row justify-between items-center md:items-start md:max-w-[1200px] mx-auto'>
         <div className='content flex flex-col md:justify-start justify-between items-center flex-wrap md:w-[40%]'>
         <div className='block text-white text-2xl md:text-5xl font-bold leading-10 md:pb-4 p-2  border-b-4 border-violet-800 md:w-[400px] w-[200px] md:text-left text-center mx-auto md:mx-2'>Contact me</div>
           <p className='my-8 w-[60%]'>I would like to hear about your project and how I could help you. Please fill in the form, and I’ll get back to you as soon as possible.</p>
